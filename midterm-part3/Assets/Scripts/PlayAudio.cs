67248dd1a1a3c4012c2d3b4d2299674ad84c6d25
Jacky12898 <32112544+Jacky12898@users.noreply.github.com>
2019-10-15 19:25:21 -0600

    resize and test audio

---
+++ midterm-part3/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource AudioClip;
+    public float Semitones;
+
+    void Start()
+    {
+        // 1.05946 ^ n changes pitch of audioclip by n semitones
+        AudioClip.pitch += Mathf.Pow(1.05946f, Semitones);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Point")
+        {
+            AudioClip.Play();
+        }
+    }
+}
