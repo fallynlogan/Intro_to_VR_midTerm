0b3022e7837e0240fe810e10690807ffd844d61b
Fallyn Logan <falo7122@colorado.edu>
2019-10-30 16:08:03 -0600

    stuff

---
+++ midterm-part3/Assets/playAudio2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSource : MonoBehaviour
+{
+    public AudioSource AudioClip;
+  
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Point")
+        {
+            Debug.Log(other.tag);
+            AudioClip.Play();
+        }
+    }
+}
